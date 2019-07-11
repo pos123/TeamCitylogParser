bc8e14c6025f08df2c43ef7c1265a39538fa07fd
Nadeem Iqbal <nadeem.iqbal@hotmail.co.uk>
2019-07-11 22:19:40 +0100

    Initial commit

--- TeamCityLogParser/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TeamCityLogParser
-{
-    public class Class1
-    {
-    }
-}
---
+++ TeamCityLogParser/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamCityLogParser
+{
+    class Parser
+    {
+        // Accepts a string of data
+        // entry has line data, line number - attached project 
+        // 
+    }
+}
