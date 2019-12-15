d6a13c47faa965530738ee13928522aa141f3354
Nadeem Iqbal <nadeem.iqbal@hotmail.co.uk>
2019-12-15 10:05:02 +0000

    Removed space from start

--- TeamCitylogParser.Test/ValueExtractorTest.cs
+++ TeamCitylogParser.Test/ValueExtractorTest.cs
@@ -10,7 +10,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectDefinition_ShouldProduceProjectDefinition()
         {
-            var dataService = new DataService(" [10:54:44] :          [exec] 158>------ Build started: Project: StarWinForms x x x, Configuration: Release Win32 ------");
+            var dataService = new DataService("[10:54:44] :          [exec] 158>------ Build started: Project: StarWinForms x x x, Configuration: Release Win32 ------");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectDefinitionEntry = EntryFactory.CreateProjectDefinitionEntryFunc(1)(valueExtractor, dataService);
@@ -24,7 +24,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexSolutionStart_ShouldExtractSolutionStartDefinition()
         {
-            var dataService = new DataService(" [10:54:44] :          [exec] Build Acceleration Console 8.0.1 (build 1867)");
+            var dataService = new DataService("[10:54:44] :          [exec] Build Acceleration Console 8.0.1 (build 1867)");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var solutionStartEntry = EntryFactory.CreateSolutionStartEntryFunc(1)(valueExtractor, dataService);
@@ -37,7 +37,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexSolutionEndBuildSucceeded_ShouldExtractSolutionBuildSucceededDefinition()
         {
-            var dataService = new DataService(" [10:54:44] :          [exec] ========== Build: 35 succeeded, 10 failed, 5 up-to-date, 326 skipped ==========");
+            var dataService = new DataService("[10:54:44] :          [exec] ========== Build: 35 succeeded, 10 failed, 5 up-to-date, 326 skipped ==========");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var solutionBuildSucceededEntry = EntryFactory.CreateSolutionEndBuildSucceededEntryFunc(1)(valueExtractor, dataService);
@@ -53,7 +53,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexSolutionEndRebuildSucceeded_ShouldExtractSolutionRebuildSucceededDefinition()
         {
-            var dataService = new DataService(" [10:54:44] :          [exec] ========== Rebuild All: 35 succeeded, 10 failed, 326 skipped ==========");
+            var dataService = new DataService("[10:54:44] :          [exec] ========== Rebuild All: 35 succeeded, 10 failed, 326 skipped ==========");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var solutionRebuildSucceededEntry = EntryFactory.CreateSolutionEndRebuildSucceededEntryFunc(1)(valueExtractor, dataService);
@@ -68,7 +68,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexSolutionEndBuildFailed_ShouldExtractSolutionEndBuildFailedDefinition()
         {
-            var dataService = new DataService(" [10:54:44]W:          [NAnt output] BUILD FAILED - 8 non-fatal error(s), 15 warning(s)");
+            var dataService = new DataService("[10:54:44]W:          [NAnt output] BUILD FAILED - 8 non-fatal error(s), 15 warning(s)");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var solutionEndBuildFailed = EntryFactory.CreateSolutionEndBuildFailedEntryFunc(1)(valueExtractor, dataService);
@@ -82,7 +82,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectEntry_ShouldExtractProjectEntryDefinition()
         {
-            var dataService = new DataService(" [10:53:29] :            [exec] 44>  blah blah blah");
+            var dataService = new DataService("[10:53:29] :            [exec] 44>  blah blah blah");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectEntry = EntryFactory.CreateProjectEntryFunc(1)(valueExtractor, dataService);
@@ -96,7 +96,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectEmptyEntry_ShouldExtractProjectEmptyEntryDefinition()
         {
-            var dataService = new DataService(" [10:53:29] :            [exec] 44>");
+            var dataService = new DataService("[10:53:29] :            [exec] 44>");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectEmptyEntry = EntryFactory.CreateProjectEmptyEntryFunc(1)(valueExtractor, dataService);
@@ -109,7 +109,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectEndEntry_ShouldExtractProjectEndEntryDefinition()
         {
-            var dataService = new DataService(" [19:07:17] :          [exec] 126>Time Elapsed 00:00:14.56");
+            var dataService = new DataService("[19:07:17] :          [exec] 126>Time Elapsed 00:00:14.56");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectEndEntry = EntryFactory.CreateProjectEndEntryFunc(1)(valueExtractor, dataService);
@@ -123,7 +123,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectEndBuildFailedEntry_ShouldExtractProjectEndBuildFailedEntryDefinition()
         {
-            var dataService = new DataService(" [19:07:17] :       [exec] 27>Build FAILED.");
+            var dataService = new DataService("[19:07:17] :       [exec] 27>Build FAILED.");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectEndBuildFailed = EntryFactory.CreateProjectEndBuildFailedEntryFunc(1)(valueExtractor, dataService);
@@ -137,7 +137,7 @@ namespace TeamCityLogParser.Test
         [Fact]
         public void GivenRegexProjectEndBuildSucceededEntry_ShouldExtractProjectEndBuildSucceededEntryDefinition()
         {
-            var dataService = new DataService(" [19:07:17] :       [exec] 54>Build succeeded.");
+            var dataService = new DataService("[19:07:17] :       [exec] 54>Build succeeded.");
             var dataDictionary = new DataDictionary();
             var valueExtractor = new ValueExtractor(dataDictionary);
             var projectEndBuildSucceeded = EntryFactory.CreateProjectEndBuildSucceededEntryFunc(1)(valueExtractor, dataService);
@@ -149,3 +149,4 @@ namespace TeamCityLogParser.Test
         }
     }
 }
+s
